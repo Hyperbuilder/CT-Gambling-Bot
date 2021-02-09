d0f553eb8e628dc1f109704e12a0cd180cba5079
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-02-09 12:48:03 +0100

    Handler system

--- economy/balance.js
+++ economy/balance.js
@@ -1,9 +1,9 @@
-module.exports = {
-commands : ['balance', 'bal'],
-maxArgs: 1,
-expectedArgs: "[Target user's @]",
-callback:  (message) => {
-const target = message.mentions.user.first() || message.author
-const targetId = target.id
-}
-}
+// module.exports = {
+// commands : ['balance', 'bal'],
+// maxArgs: 1,
+// expectedArgs: "[Target user's @]",
+// callback:  (message) => {
+// const target = message.mentions.user.first() || message.author
+// const targetId = target.id
+// }
+// }
--- main.js
+++ main.js
@@ -1,34 +1,13 @@
 const Discord = require("discord.js")
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]})
+const client = new Discord.Client()
 
 const config = require('./config.json')
 const prefix = config.prefix
 
-const fs = require('fs')
 
-client.command = new Discord.Collection();
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
 
-
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
-for(const file of commandFiles){
-const command = require(`./commands/${file}`);
-
-client.command.set(command.name, command);
-}
-
-
-client.on('ready', () => {
-    console.log("CT Gambling Bot started")
-})
-
-client.on('message', message =>{
-if(!message.content.startsWith(prefix) || message.author.bot) return;
-
-const args = message.content.slice(prefix.length).split(/ +/);
-const command = args.shift().toLowerCase();
-
-if(command === 'ping'){
-client.commands.get('ping').execute(message, args);
-}
-
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord)
 })
