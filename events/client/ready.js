03753fb3794a12f33c1e40f0dd3b738cf7bf0479
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-02-10 14:24:20 +0100

    Add MongoDB

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,13 @@
-module.exports = () => {
+const mongo = require("../../mongo")
+
+module.exports = async () => {
     console.log(`CT Gamble bot is running`)
+
+    await mongo().then(mongoose => {
+        try {
+            console.log('Connected to MongoDB')
+        } finally {
+            mongoose.connection.close()
+        }
+    })
 }
